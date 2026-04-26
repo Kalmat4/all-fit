95553a16f07a9c7aaf86f6d51d75b6074b1f91bf
Almat <59765743+Kalmat4@users.noreply.github.com>
2026-04-26 22:17:06 +0500

    add PWA support

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,7 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="theme-color" content="#212529">
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="All-Fit">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title inertia>{{ config('app.name', 'Workout App') }}</title>
@@ -9,7 +16,9 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
 </head>
+
 <body>
     @inertia
 </body>
+
 </html>
